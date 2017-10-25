55dc080f1427f9abdc7f06a4d2960768c9d1ffa9
wangyuan <coderwy@163.com>
2017-10-25 14:47:43 +0800

    test pass

---
+++ src/main/java/com/qianyang/WebInitializer.java
@@ -0,0 +1,28 @@
+package com.qianyang;
+
+import com.qianyang.config.Config;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+/**
+ * 代替 web.xml, 初始化 DispatchServlet
+ */
+public class WebInitializer implements WebApplicationInitializer{
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx
+                = new AnnotationConfigWebApplicationContext();
+        ctx.register(Config.class);
+        ctx.setServletContext(servletContext);
+
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher",
+                new DispatcherServlet(ctx));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
+    }
+}
